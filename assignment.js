d407700d244e04ae277b8f76551358f3a31aafda
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-09-07 10:51:39 -0700

    init

---
+++ assignment.js
@@ -0,0 +1,4 @@
+// 1. Implement an Array class from scratch.
+// Walk through each step of implementing an array. Don't rush through this by copying and pasting the code samples. After you've walked through it and you understand the code of the Array class, hide the sample code and try writing the Array class from scratch using the memory module here for allocating memory.
+
+// Be sure to export the memory module and then import it using require.
